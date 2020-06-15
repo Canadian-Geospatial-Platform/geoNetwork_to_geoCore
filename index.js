7cc9ae52ff35f5933f1a4669da735e54a3d8fa7a
indiciumx <chris@indiciumx.com>
2020-06-15 10:32:46 -0400

    200615

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
 exports.handler = async (event) => {
 
-  var transform = require("node-json-transform").transform;
-  // or
-  var { transform } = require("node-json-transform");
+function getS3Objects(bucket,key){
+    return s3.getObject({
+      Bucket:'dev-geocore-transform-input-1',
+      Key:'f511dd05-2890-469a-9331-29a615b59761.geojson',
+      ResponseContentType:'application/json'})
+               .promise().then(file=>{return file})
+               .catch(error =>{return error});
+}
 
-  var result = transform({
-    text: "hello"
-  }, {
-    item: {
-      message: "text"
-    }
-  });
+//  var transform = require("node-json-transform").transform;
+  // or
+//  var { transform } = require("node-json-transform");
+
+//  var result = transform({
+//    text: "hello",
+//    night: "Day"
+//  }, {
+//    item: {
+//      message: "text",
+//      time: "night"
+//    }
+//  });
 
     // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(result),
-    };
-    return response;
+//    const response = {
+//        statusCode: 200,
+//        body: result,
+//    };
+//   return response;
 };
